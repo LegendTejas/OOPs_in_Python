885fc5185639d101afbf0881992746c2359ab905
Tejas Tp <tejastp193@gmail.com>
2025-08-15 17:55:48 +0530

    Create 14_Part02_Special_Methods.py

---
+++ OOPs_Advanced/14_Part02_Special_Methods.py
@@ -0,0 +1,23 @@
+# 3. Arithmetic Operator Overloading
+# __add__, __sub__, __mul__, etc.
+
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __str__(self):
+        return f"Vector({self.x}, {self.y})"
+
+# Using overloaded operators
+v1 = Vector(3, 4)
+v2 = Vector(1, 2)
+
+print(v1 + v2)  # Vector(4, 6)
+print(v1 - v2)  # Vector(2, 2)
